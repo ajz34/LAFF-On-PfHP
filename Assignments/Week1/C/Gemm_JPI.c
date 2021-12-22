cacc6666b86f30e9d9402b9b8695c59968ae045d
ajz34 <ajz34@outlook.com>
2021-12-22 19:55:02 +0800

    Week 1 complete

--- Assignments/Week1/C/Gemm_JPI.c
+++ Assignments/Week1/C/Gemm_JPI.c
@@ -2,14 +2,12 @@
 #define beta( i,j )  B[ (j)*ldB + i ]   // map beta( i,j )  to array B
 #define gamma( i,j ) C[ (j)*ldC + i ]   // map gamma( i,j ) to array C
 
-void MyGemv( int, int, double *, int, double *, int, double *, int );
-
-void MyGemm( int m, int n, int k,
-	     double *A, int ldA,
-	     double *B, int ldB,
-	     double *C, int ldC )
+void MyGemm( int m, int n, int k, double *A, int ldA,
+	     double *B, int ldB, double *C, int ldC )
 {
   for ( int j=0; j<n; j++ )
-    MyGemv(m, n, A, ldA, &beta(0,j), 1, &gamma(0,j), 1);
+  for ( int p=0; p<k; p++ )
+  for ( int i=0; i<m; i++ )
+    gamma( i,j ) += alpha( i,p ) * beta( p,j );
 }
   
